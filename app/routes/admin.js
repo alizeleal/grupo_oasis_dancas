fe1ec8ccfa5f458975bbedacd9a70293db50f572
Alize Leal Pereira <48735281+alizeleal@users.noreply.github.com>
2025-04-08 03:17:38 -0300

    includes body parser

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,4 +3,12 @@ module.exports= function(app){
 app.get('/adm/incluirnoticia', function(req, res){
     res.render("adm/incluir")
 
-})}
+});
+
+app.post('/noticias/salvar', function(req, res){
+    let noticias =  req.body;
+    res.send(noticias)
+
+});
+
+}
--- config/server.js
+++ config/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const consign = require("consign");
+const parser = require("body-parser");
 
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/public'));
+app.use(parser.urlencoded({extended: true}));
 
 app.set('view engine','ejs');
 app.set('views', './app/views');
